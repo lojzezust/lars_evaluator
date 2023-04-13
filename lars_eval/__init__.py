ab95fdd0351b471f413ba805d014a91acdc16d3c
Lojze Žust <lojze.zust@fri.uni-lj.si>
2023-04-13 12:29:25 +0000

    Import get_cfg

--- lars_eval/__init__.py
+++ lars_eval/__init__.py
@@ -1,3 +1,5 @@
 
 __all__ = ['config', 'context', 'evaluator', 'metrics', 'panoptic', 'utils']
+
 from lars_eval.evaluator import SemanticEvaluator, PanopticEvaluator
+from lars_eval.config import get_cfg
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lars_evaluator",
-    version="0.1.1",
+    version="0.1.2",
     author="Lojze Žust",
     author_email="lojze.zust@fri.uni-lj.si",
     description="Evaluation toolkit for the LaRS dataset.",
